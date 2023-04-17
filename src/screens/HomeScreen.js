8a0f7688300f7ad00997440b1e5c60b43904ffa0
ayoubjouda <ayoubjouda@hotmail.fr>
2023-04-17 21:22:22 +0000

    start

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,150 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList,TextInput } from 'react-native';
-import COLORS from '../constants/colors';
-import FONTS from '../constants/fonts';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ScrollView, FlatList, TextInput } from "react-native";
+import COLORS from "../constants/colors";
+import FONTS from "../constants/fonts";
 import GenreCard from "../components/GenreCard";
-import Cards from './Cards';
-import MovieCard from '../components/MovieCard';
-import ItemSeparator from '../components/ItemSeparator';
-import { getNowPlayingMovies, getUpcomingMovies,getPopularSeries,getUpcomingSeries} from '../services/MovieService';
-import { Feather,Ionicons } from '@expo/vector-icons';
-const  HomeScreen = ({navigation}) => {
-  
+import Cards from "./Cards";
+import MovieCard from "../components/MovieCard";
+import ItemSeparator from "../components/ItemSeparator";
+import { getNowPlayingMovies, getUpcomingMovies, getPopularSeries, getUpcomingSeries } from "../services/MovieService";
+import { Feather, Ionicons } from "@expo/vector-icons";
+const HomeScreen = ({ navigation }) => {
   //const [activeGenre, setActiveGenre] = useState("All");
-  const [nowPlayingMovies, setNowPlayingMovies]= useState({});
-  const [upcomingMovies, setUpcomingMovies]= useState({});
-  const [PopularSeries, setPopularSeries]= useState({});
-  const [upcomingSeries, setUpcomingSeries]= useState({});
+  const [nowPlayingMovies, setNowPlayingMovies] = useState({});
+  const [upcomingMovies, setUpcomingMovies] = useState({});
+  const [PopularSeries, setPopularSeries] = useState({});
+  const [upcomingSeries, setUpcomingSeries] = useState({});
   // const [getGenres, setGenres]= useState([{id:10110 ,name:"All"}]);
 
   useEffect(() => {
-    getNowPlayingMovies().then((movieResponse => 
-      setNowPlayingMovies(movieResponse.data)));
+    getNowPlayingMovies().then((movieResponse) => setNowPlayingMovies(movieResponse.data));
 
-     getUpcomingMovies().then((movieResponse => 
-      setUpcomingMovies(movieResponse.data)));
+    getUpcomingMovies().then((movieResponse) => setUpcomingMovies(movieResponse.data));
 
-      getPopularSeries().then((tvResponse => 
-        setPopularSeries(tvResponse.data)));
+    getPopularSeries().then((tvResponse) => setPopularSeries(tvResponse.data));
 
-        getUpcomingSeries().then((tvResponse => 
-          setUpcomingSeries(tvResponse.data)));
-      
-    // getAllGenres().then((genreResponse => 
+    getUpcomingSeries().then((tvResponse) => setUpcomingSeries(tvResponse.data));
+
+    // getAllGenres().then((genreResponse =>
     //   setGenres([...getGenres, ...genreResponse.data.genres])));
   }, []);
 
   return (
     <ScrollView style={styles.container}>
-      <StatusBar
-        style="auto"
-        translucent={false}
-        backgroundColor={COLORS.BASIC_BACKROUND} />
+      <StatusBar style="auto" translucent={false} backgroundColor={COLORS.BASIC_BACKROUND} />
       <View style={styles.headerContainer}>
         <Text style={styles.headerTitle}>Découvrir</Text>
-        <Feather
-          name="search"
-          size={34}
-          color={COLORS.ACTIVE}
-          onPress={() => navigation.navigate('Search')}
-        />
+        <Feather name="search" size={34} color={COLORS.ACTIVE} onPress={() => navigation.navigate("Search")} />
       </View>
-      <View style={styles.genreListContainer}>
-      </View>
+      <View style={styles.genreListContainer}></View>
       <FlatList
-      data={nowPlayingMovies.results} 
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.id.toString()}
-      ItemSeparatorComponent={() => <ItemSeparator width={20} />}
-      ListHeaderComponent={() => <ItemSeparator width={20}/>}
-      ListFooterComponent={() => <ItemSeparator width={20}/>}
-      renderItem={({item}) => ( 
-      <MovieCard 
-        title={item.title}
-        language={item.original_language}
-        voteAverage={item.vote_average}
-        voteCount={item.vote_count}
-        poster={item.poster_path}
-        heartLess={false}
-        onPress={() => navigation.navigate("movie",{movieId:item.id})}
-
-      />)}
+        data={nowPlayingMovies.results}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+        ListHeaderComponent={() => <ItemSeparator width={20} />}
+        ListFooterComponent={() => <ItemSeparator width={20} />}
+        renderItem={({ item }) => (
+          <MovieCard
+            title={item.title}
+            language={item.original_language}
+            voteAverage={item.vote_average}
+            voteCount={item.vote_count}
+            poster={item.poster_path}
+            heartLess={false}
+            onPress={() => navigation.navigate("movie", { movieId: item.id })}
+          />
+        )}
       />
       <View style={styles.headerContainer}>
         <Text style={styles.headerTitle}>Bientôt disponible</Text>
       </View>
       <FlatList
-      data={upcomingMovies.results} 
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.id.toString()}
-      ItemSeparatorComponent={() => <ItemSeparator width={20} />}
-      ListHeaderComponent={() => <ItemSeparator width={20}/>}
-      ListFooterComponent={() => <ItemSeparator width={20}/>}
-      renderItem={({item}) => ( 
-      <MovieCard 
-        title={item.title}
-        language={item.original_language}
-        voteAverage={item.vote_average}
-        voteCount={item.vote_count}
-        poster={item.poster_path}
-        size={0.7}
-        onPress={() => navigation.navigate("movie",{movieId:item.id})}
-      />)}
-      />  
-
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerTitle}>Séries populaires</Text>
+        data={upcomingMovies.results}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+        ListHeaderComponent={() => <ItemSeparator width={20} />}
+        ListFooterComponent={() => <ItemSeparator width={20} />}
+        renderItem={({ item }) => (
+          <MovieCard
+            title={item.title}
+            language={item.original_language}
+            voteAverage={item.vote_average}
+            voteCount={item.vote_count}
+            poster={item.poster_path}
+            size={0.7}
+            onPress={() => navigation.navigate("movie", { movieId: item.id })}
+          />
+        )}
+      />
+      <View style={{ paddingBottom: 100 }}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerTitle}>Séries populaires</Text>
+        </View>
+        <FlatList
+          data={PopularSeries.results}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+          ListHeaderComponent={() => <ItemSeparator width={20} />}
+          ListFooterComponent={() => <ItemSeparator width={20} />}
+          renderItem={({ item }) => (
+            <MovieCard
+              title={item.title}
+              language={item.original_language}
+              voteAverage={item.vote_average}
+              voteCount={item.vote_count}
+              poster={item.poster_path}
+              heartLess={false}
+              onPress={() => navigation.navigate("serie", { serieId: item.id })}
+            />
+          )}
+        />
       </View>
-      <FlatList
-      data={PopularSeries.results} 
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.id.toString()}
-      ItemSeparatorComponent={() => <ItemSeparator width={20} />}
-      ListHeaderComponent={() => <ItemSeparator width={20}/>}
-      ListFooterComponent={() => <ItemSeparator width={20}/>}
-      renderItem={({item}) => ( 
-      <MovieCard 
-        title={item.title}
-        language={item.original_language}
-        voteAverage={item.vote_average}
-        voteCount={item.vote_count}
-        poster={item.poster_path}
-        heartLess={false}
-        onPress={() => navigation.navigate("serie",{serieId:item.id})}
-
-      />)}
-      />  
-
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerTitle}>Nouvelles Séries</Text>
-      </View>
-      <FlatList
-      data={upcomingSeries.results} 
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.id.toString()}
-      ItemSeparatorComponent={() => <ItemSeparator width={20} />}
-      ListHeaderComponent={() => <ItemSeparator width={20}/>}
-      ListFooterComponent={() => <ItemSeparator width={20}/>}
-      renderItem={({item}) => ( 
-      <MovieCard 
-        title={item.title}
-        language={item.original_language}
-        voteAverage={item.vote_average}
-        voteCount={item.vote_count}
-        poster={item.poster_path}
-        size={0.7}
-        onPress={() => navigation.navigate("serie",{serieId:item.id})}
-      />)}
-      />     
     </ScrollView>
-
-    
-    
   );
 };
 
@@ -152,6 +114,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.BASIC_BACKROUND,
+    paddingTop: 35,
   },
   headerContainer: {
     flexDirection: "row",
@@ -163,17 +126,14 @@ const styles = StyleSheet.create({
   headerTitle: {
     fontSize: 28,
     fontFamily: FONTS.REGULAR,
-    
   },
   headerSubTitle: {
     fontSize: 17,
     color: COLORS.ACTIVE,
-    fontFamily:FONTS.BOLD,
-    
+    fontFamily: FONTS.BOLD,
   },
-  genreListContainer:
-  {
-    paddingVertical: 10
-  }
+  genreListContainer: {
+    paddingVertical: 10,
+  },
 });
 export default HomeScreen;
--- src/screens/SearchBar.js
+++ src/screens/SearchBar.js
@@ -1,104 +1,103 @@
-import react,{useState,useEffect} from 'react';
-import axios from 'axios';
-import { StyleSheet,View,Text,TextInput,TouchableOpacity,StatusBar,Dimensions,FlatList,Button} from 'react-native';
-import {Feather } from "@expo/vector-icons";
-import COLORS from '../constants/colors';
-import fonts from '../constants/fonts';
-import { getSearchMovie,getPopularMovies } from '../services/MovieService';
-import MovieCard from '../components/MovieCard';
-import ItemSeparator from '../components/ItemSeparator';
+import react, { useState, useEffect } from "react";
+import axios from "axios";
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, StatusBar, Dimensions, FlatList, Button } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import COLORS from "../constants/colors";
+import fonts from "../constants/fonts";
+import { getSearchMovie, getPopularMovies } from "../services/MovieService";
+import MovieCard from "../components/MovieCard";
+import ItemSeparator from "../components/ItemSeparator";
 
+const SearchBar = ({ navigation }) => {
+  const [GetSearchMovie, SetSearchMovie] = useState([]);
+  const [search, setSearch] = useState();
 
-const SearchBar = ({navigation}) => {
+  function GetMovies(s) {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/multi?api_key=951e3639ab5ba5d05cbda770ec2aacbe&language=fr&query=${s}&page=1&include_adult=false`
+      )
+      .then((data) => SetSearchMovie(data.data.results))
+      .catch((err) => console.log(err));
+  }
 
-    const [GetSearchMovie,SetSearchMovie]=useState([]);
-    const [search,setSearch]=useState();
-
-    function GetMovies(s){
-        axios.get(`
-        https://api.themoviedb.org/3/search/multi?api_key=951e3639ab5ba5d05cbda770ec2aacbe&language=fr&query=${s}&page=1&include_adult=false`).
-        then((data)=>SetSearchMovie(data.data.results)).catch((err)=>console.log(err))
-    }
-    
-    return(
-        <View style={styles.Container}>
-        <View style={styles.headerContainer}>
-            <TouchableOpacity 
-            activeOpacity={0.5} 
-            onPress={() =>navigation.goBack('home') }>
-            <Feather name="chevron-left" size={35} color={COLORS.BLACK}/>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.buttonText}>
-        <TextInput 
-        style={styles.inputText}
-        placeholder='quel est le film,genre,personne,serie que vous voulez chercher?  '
-        value={search}
-        onChangeText={(text)=>{
-            setSearch(text)
-        }}
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.goBack("home")}>
+          <Feather name="chevron-left" size={35} color={COLORS.BLACK} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonText}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="quel est le film,genre,personne,serie que vous voulez chercher?  "
+          value={search}
+          onChangeText={(text) => {
+            setSearch(text);
+          }}
         />
-        <Button title='rechercher' onPress={()=>GetMovies(search)}></Button>
-        </View>
-        
+        <Button title="rechercher" onPress={() => GetMovies(search)}></Button>
+      </View>
+      <View style={{ marginTop: 10 }}>
         <FlatList
-        data={GetSearchMovie} 
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.id.toString()}
-        ItemSeparatorComponent={() => <ItemSeparator width={20} />}
-        ListHeaderComponent={() => <ItemSeparator width={20}/>}
-        ListFooterComponent={() => <ItemSeparator width={20}/>}
-        renderItem={({item}) => ( 
-        <MovieCard 
-            title={item.title}
-            
-            voteAverage={item.vote_average}
-            voteCount={item.vote_count}
-            poster={item.poster_path}
-            heartLess={false}
-            onPress={() => navigation.navigate("movie",{movieId:item.id})}
+          data={GetSearchMovie}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+          ListHeaderComponent={() => <ItemSeparator width={20} />}
+          ListFooterComponent={() => <ItemSeparator width={20} />}
+          contentContainerStyle={{ justifyContent: "center", alignItems: "center", gap: 10 }}
+          renderItem={({ item }) => (
+            <MovieCard
+              title={item.title}
+              voteAverage={item.vote_average}
+              voteCount={item.vote_count}
+              poster={item.poster_path}
+              heartLess={false}
+              onPress={() => navigation.navigate("movie", { movieId: item.id })}
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
+};
 
-        />)}
-      />
-        
-        
-        </View>
-    );
-}
-
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:COLORS.WHITE,
-    },
-    headerContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        
-      },
-      inputText:{
-        fontSize:16,
-        textAlignVertical:'center',
-        borderWidth:1,
-        paddingLeft:10,
-        paddingStart:20,
-        margin:5,
-        borderColor:COLORS.EXTRA_LIGHT_GRAY,
-        height:50,
-        backgroundColor:COLORS.WHITE,
-        color:COLORS.BLACK,
-        fontFamily:fonts.REGULAR
-    },
-    buttonText:{
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-evenly'
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.WHITE,
+    paddingTop: 35,
+    paddingHorizontal: 10,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  inputText: {
+    fontSize: 16,
+    textAlignVertical: "center",
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: COLORS.EXTRA_LIGHT_GRAY,
+    height: 50,
+    backgroundColor: COLORS.WHITE,
+    color: COLORS.BLACK,
+    fontFamily: fonts.REGULAR,
+    maxWidth: 250,
+    minWidth: 250,
+    borderRadius: 10,
+  },
+  buttonText: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+});
 
 export default SearchBar;
